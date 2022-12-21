7488c3969ca21c3dae966259338f089d46012ea5
emiliaaslund <emilia.aslund@cmeducations.se>
2022-12-21 19:07:29 +0100

    tester

--- e2e/app.test.js
+++ e2e/app.test.js
@@ -9,10 +9,15 @@ test("test", async ({ page }) => {
     .getByRole("heading", { name: "Choose a project to get started" })
     .click();
   await page.getByRole("button", { name: "Project ​" }).click();
-  await page.getByRole("option", { name: "torsdag" }).click();
+  await page.getByRole("option", { name: "Fredag" }).click();
+  await page.getByRole("button", { name: "Task ​" }).click();
+  await page.getByRole("option", { name: "kaffe" }).click();
+  await page.getByRole("heading", { name: "Task: kaffe" }).click();
   await page.getByRole("button", { name: "Start" }).click();
-  await page.getByRole("button", { name: "Pause" }).click();
   await page.waitForTimeout(3000);
+  await page.getByRole("button", { name: "Pause" }).click();
+  await page.waitForTimeout(2000);
+  await page.getByRole("button", { name: "Save" }).click();
   await page.getByRole("link", { name: "Calendar" }).click();
   await page
     .getByRole("heading", { name: "Choose a date to se timestamps" })
@@ -24,23 +29,29 @@ test("test", async ({ page }) => {
   await page.getByRole("button", { name: "CREATE A NEW PROJECT" }).click();
   await page.getByTestId("Project").click();
   await page.getByTestId("Project").fill("Test");
+  await page.waitForTimeout(2000);
   await page.getByRole("button", { name: "Add a project" }).click();
   await page.getByRole("button", { name: "Test X" }).click();
+  await page.waitForTimeout(2000);
   await page.getByRole("tab", { name: "Tasks" }).click();
   await page.getByRole("heading", { name: "Active Tasks:" }).click();
   await page.getByRole("button", { name: "Add a new task" }).click();
   await page.getByTestId("testInput").click();
+  await page.waitForTimeout(2000);
   await page.getByTestId("testInput").fill("TestTask");
   await page.getByRole("button", { name: "Project ​" }).click();
   await page.getByRole("option", { name: "Test" }).click();
   await page.getByRole("button", { name: "ADD A TASK" }).click();
   await page.getByText("TestTask").click();
+  await page.waitForTimeout(3000);
   await page
     .getByRole("listitem")
     .filter({ hasText: "TestTaskX" })
     .getByRole("button", { name: "X" })
     .click();
+  await page.waitForTimeout(2000);
   await page.getByRole("tab", { name: "Projects" }).click();
+  await page.waitForTimeout(2000);
   await page
     .getByRole("button", { name: "Test X" })
     .getByTestId("deletebtn")
